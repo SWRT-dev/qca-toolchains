02234c3053565c96771069e04ce37fb0b441005b
ZhaoWei Xu <paldier@hotmail.com>
2021-11-05 22:53:05 +0800

    add new toolchains

---
+++ openwrt-gcc520_musl.arm/lib/gcc/arm-openwrt-linux-muslgnueabi/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/QSDK/qsdk/build_dir/toolchain-arm_cortex-a7_gcc-5.2.0_musl-1.1.16_eabi/gcc-5.2.0/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt GCC 5.2.0 6ff3409+r49254' --prefix=/opt/openwrt-gcc520_musl.arm --build=i686-linux-gnu --host=i686-linux-gnu --target=arm-openwrt-linux-muslgnueabi --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/QSDK/qsdk/staging_dir/host --with-mpfr=/QSDK/qsdk/staging_dir/host --with-mpc=/QSDK/qsdk/staging_dir/host --disable-decimal-float --enable-libssp --enable-__cxa_atexit --with-arch=armv7-a --with-headers=/opt/openwrt-gcc520_musl.arm/include --disable-libsanitizer --enable-languages=c,c++ --enable-shared --enable-threads --with-slibdir=/opt/openwrt-gcc520_musl.arm/lib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-a" }, { "float", "soft" }, { "tls", "gnu" } };
