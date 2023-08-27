4cf9cac9c9bd99a3d60a3cb551fd110c589c4479
ZhaoWei Xu <paldier@hotmail.com>
2023-08-27 10:04:07 +0800

    add new toolchain for qsdk12.2

---
+++ toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl/lib/gcc/aarch64-openwrt-linux-musl/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/paldier/qsdk/qsdk12/qsdk/build_dir/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl/gcc-8.3.0/configure --with-bugurl=http://bugs.openwrt.org/ --with-pkgversion='OpenWrt GCC 8.3.0 r0+12834-6a10c44cf5' --prefix=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu --target=aarch64-openwrt-linux-musl --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-libmpx --disable-nls --without-isl --without-cloog --with-host-libstdcxx=-lstdc++ --with-gmp=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/host --with-mpfr=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/host --with-mpc=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/host --disable-decimal-float --with-diagnostics-color=auto-if-env --disable-libssp --enable-__cxa_atexit --with-arch=armv8-a --with-headers=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl/include --disable-libsanitizer --enable-languages=c,c++ --enable-shared --enable-threads --with-slibdir=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/toolchain-aarch64_cortex-a53+neon-vfpv4_gcc-8.3.0_musl/lib --enable-lto --with-libelf=/home/paldier/qsdk/qsdk12/qsdk/staging_dir/host";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv8-a" } };
